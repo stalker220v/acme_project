f3edfe6a75ebd1edbce8623d1493ae8a71245c57
Valentin Bobkov <ya333@yandex.ru>
2024-06-25 21:03:54 +0300

    add class before mixin

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'birthday'
 
 urlpatterns = [
-    path('', views.birthday, name='create'),
+    path('', views.BirthdayCreateView.as_view(), name='create'),
     # Новый маршрут.
-    path('list/', views.birthday_list, name='list'),
+    path('list/', views.BirthdayListView.as_view(), name='list'),
     # Маршрут для редактирования.
-    path('<int:pk>/edit/', views.birthday, name='edit'),
-    path('<int:pk>/delete/', views.delete_birthday, name='delete'),
+    path('<int:pk>/edit/', views.BirthdayUpdateView.as_view(), name='edit'),
+    path('<int:pk>/delete/', views.BirthdayDeleteView.as_view(), name='delete'),
 ]
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -12,86 +12,134 @@ from .utils import calculate_birthday_countdown
 # Импортируем модель дней рождения.
 from .models import Birthday
 
-from django.views.generic import ListView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.urls import reverse_lazy
 
-def birthday_list(request):
-    # Получаем список всех объектов с сортировкой по id.
-    birthdays = Birthday.objects.order_by('id')
-    # Создаём объект пагинатора с количеством 10 записей на страницу.
-    paginator = Paginator(birthdays, 10)
 
-    # Получаем из запроса значение параметра page.
-    page_number = request.GET.get('page')
-    # Получаем запрошенную страницу пагинатора.
-    # Если параметра page нет в запросе или его значение не приводится к числу,
-    # вернётся первая страница.
-    page_obj = paginator.get_page(page_number)
-    # Вместо полного списка объектов передаём в контекст
-    # объект страницы пагинатора
+# Создаём миксин.
+class BirthdayMixin:
+    model = Birthday
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+    success_url = reverse_lazy('birthday:list')
 
-    # Передаём их в контекст шаблона.
-    context = {'page_obj': page_obj}
-    return render(request, 'birthday/birthday_list.html', context)
+
+# Наследуем класс от встроенного ListView:
+class BirthdayListView(ListView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # ...сортировку, которая будет применена при выводе списка объектов:
+    ordering = 'id'
+    # ...и даже настройки пагинации:
+    paginate_by = 10
+
+class BirthdayCreateView(BirthdayMixin, CreateView):
+    # # Все атрибуты унаследованы от BirthdayMixin, поэтому создали класс и всё)
+    pass
+    # # Указываем модель, с которой работает CBV...
+    # model = Birthday
+    # # Этот класс сам может создать форму на основе модели!
+    # # Нет необходимости отдельно создавать форму через ModelForm.
+    # # Указываем поля, которые должны быть в форме:
+    # # используем готовую часть формы BirthdayForm с виджетами из формс
+    # form_class = BirthdayForm
+    # # Явным образом указываем шаблон:
+    # template_name = 'birthday/birthday.html'
+    # # Указываем namespace:name страницы, куда будет перенаправлен пользователь
+    # # после создания объекта:
+    # success_url = reverse_lazy('birthday:list')
+
+class BirthdayUpdateView(UpdateView):
+    """Редактирование записей"""
+    # И здесь все атрибуты наследуются от BirthdayMixin.
+    pass
+    # model = Birthday
+    # form_class = BirthdayForm
+    # template_name = 'birthday/birthday.html'
+    # success_url = reverse_lazy('birthday:list')
+
+# def birthday_list(request):
+#     # Получаем список всех объектов с сортировкой по id.
+#     birthdays = Birthday.objects.order_by('id')
+#     # Создаём объект пагинатора с количеством 10 записей на страницу.
+#     paginator = Paginator(birthdays, 10)
+#
+#     # Получаем из запроса значение параметра page.
+#     page_number = request.GET.get('page')
+#     # Получаем запрошенную страницу пагинатора.
+#     # Если параметра page нет в запросе или его значение не приводится к числу,
+#     # вернётся первая страница.
+#     page_obj = paginator.get_page(page_number)
+#     # Вместо полного списка объектов передаём в контекст
+#     # объект страницы пагинатора
+#
+#     # Передаём их в контекст шаблона.
+#     context = {'page_obj': page_obj}
+#     return render(request, 'birthday/birthday_list.html', context)
 
 # Добавим опциональный параметр pk.
-def birthday(request, pk=None):
-    # Если в запросе указан pk (если получен запрос на редактирование объекта):
-    if pk is not None:
-        # Получаем объект модели или выбрасываем 404 ошибку.
-        instance = get_object_or_404(Birthday, pk=pk)
-    # Если в запросе не указан pk
-    # (если получен запрос к странице создания записи):
-    else:
-        # Связывать форму с объектом не нужно, установим значение None.
-        instance = None
-    # Передаём в форму либо данные из запроса, либо None.
-    # В случае редактирования прикрепляем объект модели.
+# def birthday(request, pk=None):
+#     # Если в запросе указан pk (если получен запрос на редактирование объекта):
+#     if pk is not None:
+#         # Получаем объект модели или выбрасываем 404 ошибку.
+#         instance = get_object_or_404(Birthday, pk=pk)
+#     # Если в запросе не указан pk
+#     # (если получен запрос к странице создания записи):
+#     else:
+#         # Связывать форму с объектом не нужно, установим значение None.
+#         instance = None
+#     # Передаём в форму либо данные из запроса, либо None.
+#     # В случае редактирования прикрепляем объект модели.
+#
+#     # Этот код гораздо короче, а работает точно так же, как и предыдущий вариант.
+#     # Весь фокус в выражении BirthdayForm(request.GET or None).
+#     # Его логика такова: если в GET-запросе были переданы параметры — значит,
+#     # объект request.GET не пуст и этот объект передаётся в форму;
+#     # если же объект request.GET пуст — срабатывает условиe or и форма
+#     # создаётся без параметров, через BirthdayForm(None) — это идентично
+#     # обычному BirthdayForm().
+#     form = BirthdayForm(
+#         request.POST or None,
+#         # Файлы, переданные в запросе, указываются отдельно.
+#         files=request.FILES or None,
+#         instance=instance
+#     )
+#     # Создаём словарь контекста сразу после инициализации формы.
+#     context = {'form': form}
+#     # Если форма валидна...
+#     if form.is_valid():
+#         # В классе ModelForm есть встроенный метод save(),
+#         # он позволяет сохранить данные из формы в БД.
+#         # После сохранения метод save() возвращает сохранённый
+#         # объект — это можно использовать для подтверждения,
+#         # что сохранение данных прошло успешно.
+#         # Добавим строчку с вызовом метода save():
+#         form.save()
+#         # ...вызовем функцию подсчёта дней:
+#         birthday_countdown = calculate_birthday_countdown(
+#             # ...и передаём в неё дату из словаря cleaned_data.
+#             form.cleaned_data['birthday']
+#         )
+#         # Обновляем словарь контекста: добавляем в него новый элемент.
+#         context.update({'birthday_countdown': birthday_countdown})
+#     return render(request, 'birthday/birthday.html', context)
 
-    # Этот код гораздо короче, а работает точно так же, как и предыдущий вариант.
-    # Весь фокус в выражении BirthdayForm(request.GET or None).
-    # Его логика такова: если в GET-запросе были переданы параметры — значит,
-    # объект request.GET не пуст и этот объект передаётся в форму;
-    # если же объект request.GET пуст — срабатывает условиe or и форма
-    # создаётся без параметров, через BirthdayForm(None) — это идентично
-    # обычному BirthdayForm().
-    form = BirthdayForm(
-        request.POST or None,
-        # Файлы, переданные в запросе, указываются отдельно.
-        files=request.FILES or None,
-        instance=instance
-    )
-    # Создаём словарь контекста сразу после инициализации формы.
-    context = {'form': form}
-    # Если форма валидна...
-    if form.is_valid():
-        # В классе ModelForm есть встроенный метод save(),
-        # он позволяет сохранить данные из формы в БД.
-        # После сохранения метод save() возвращает сохранённый
-        # объект — это можно использовать для подтверждения,
-        # что сохранение данных прошло успешно.
-        # Добавим строчку с вызовом метода save():
-        form.save()
-        # ...вызовем функцию подсчёта дней:
-        birthday_countdown = calculate_birthday_countdown(
-            # ...и передаём в неё дату из словаря cleaned_data.
-            form.cleaned_data['birthday']
-        )
-        # Обновляем словарь контекста: добавляем в него новый элемент.
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context)
+class BirthdayDeleteView(DeleteView):
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
 
-def delete_birthday(request, pk):
-    # Получаем объект модели или выбрасываем 404 ошибку.
-    instance = get_object_or_404(Birthday, pk=pk)
-    # В форму передаём только объект модели;
-    # передавать в форму параметры запроса не нужно.
-    form = BirthdayForm(instance=instance)
-    context = {'form': form}
-    # Если был получен POST-запрос...
-    if request.method == 'POST':
-        # ...удаляем объект:
-        instance.delete()
-        # ...и переадресовываем пользователя на страницу со списком записей.
-        return redirect('birthday:list')
-    # Если был получен GET-запрос — отображаем форму.
-    return render(request, 'birthday/birthday.html', context)
+# def delete_birthday(request, pk):
+#     # Получаем объект модели или выбрасываем 404 ошибку.
+#     instance = get_object_or_404(Birthday, pk=pk)
+#     # В форму передаём только объект модели;
+#     # передавать в форму параметры запроса не нужно.
+#     form = BirthdayForm(instance=instance)
+#     context = {'form': form}
+#     # Если был получен POST-запрос...
+#     if request.method == 'POST':
+#         # ...удаляем объект:
+#         instance.delete()
+#         # ...и переадресовываем пользователя на страницу со списком записей.
+#         return redirect('birthday:list')
+#     # Если был получен GET-запрос — отображаем форму.
+#     return render(request, 'birthday/birthday.html', context)
